691ab379806b8c7871f71c455e0289fea5a74c9a
sarthakpatel2 <sarthakpatelboby@gmail.com>
2025-12-18 22:42:18 +0530

    [Sarthak Patel]Add. Double Operation

---
+++ core-java-practice/gcr-codebase/programming-elements/DoubleOperation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class IntOperation{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        // first number input
+        double a=sc.nextDouble();
+        // second number input
+        double b=sc.nextDouble();
+        // third number input
+        double c=sc.nextDouble();
+        //operations
+        double operation1=a+b*c;
+        double operation2=a*b+c;
+        double operation3=c+a/b;
+        double operation4=a%b+c;
+        //output
+        System.out.println("The result of  Double operations are:"+operation1+","+operation2+","+operation3+" , and "+operation4);
+    }
+}
