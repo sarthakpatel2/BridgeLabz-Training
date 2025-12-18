56fef978118c3799975ad525db98dcc02e992de8
sarthakpatel2 <sarthakpatelboby@gmail.com>
2025-12-18 10:47:57 +0530

    [Sarthak Patel]Add. Age of Harry

---
+++ core-java-practice/gcr-codebase/programming-elements/AgeOfHarry.java
@@ -0,0 +1,12 @@
+class AgeOfHarry{
+    public static void main(String[] args) {
+        // Birth year
+        int birthYear=2000;
+        // current year
+        int currentYear=2024;
+        // calculating age
+        int age=currentYear-birthYear;
+        // print age
+        System.out.print("Harry's age in 2024 is:"+age);
+    }
+}
