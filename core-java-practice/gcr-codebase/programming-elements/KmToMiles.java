53d7c18a42e55c7ddb0ed4415545e34f67d89a06
sarthakpatel2 <sarthakpatelboby@gmail.com>
2025-12-18 11:10:25 +0530

    [Sarthak Patel]Add. Convert Kilometer To Miles

---
+++ core-java-practice/gcr-codebase/programming-elements/KmToMiles.java
@@ -0,0 +1,10 @@
+class KmToMiles{
+    public static void main(String[] args) {
+        // distance value
+        float kilometers=10.8f;
+        // conversion
+        float res=kilometers*1.6f;
+        System.out.println("Distance of"+kilometers+ "km in miles is:"+res);
+
+    }
+}
