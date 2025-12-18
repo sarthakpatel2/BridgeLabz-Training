439e56219569c350270155073789fd942b7e4c3a
sarthakpatel2 <sarthakpatelboby@gmail.com>
2025-12-18 10:59:13 +0530

    [Sarthak Patel]Add. Average Marks of Sam

---
+++ core-java-practice/gcr-codebase/programming-elements/AverageMarksOfSam.java
@@ -0,0 +1,14 @@
+class AverageMarksOfSam{
+    public static void main(String[] args){
+        // Maths marks
+        int Maths=94;
+        // Physics marks
+        int Physics=95;
+        // Chemistry marks
+        int Chemistry=96;
+        // calculating average marks
+        int avgMarks=(Maths+Physics+Chemistry)/3;
+        // print average marks
+        System.out.print("Sams's average mark in PCM is:"+avgMarks);
+    }
+}
