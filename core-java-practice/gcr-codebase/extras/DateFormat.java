1d09ca7e6b562f896a769339c17b22a80d57bd3e
sarthakpatel2 <sarthakpatelboby@gmail.com>
2025-12-28 19:28:21 +0530

    [Sarthak Patel] Add. Date Formatting

---
+++ core-java-practice/gcr-codebase/extras/DateFormat.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class DateFormat{
+    public static void main(String[] args) {
+        LocalDate today=LocalDate.now();
+        // define diff date formats
+        DateTimeFormatter format1= DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter format2=DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter format3=DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+        // display formatted dates
+        System.out.println("Format 1: "+today.format(format1));
+        System.out.println("Format 2: "+today.format(format2));
+        System.out.println("Format 3: "+today.format(format3));
+    }
+}
