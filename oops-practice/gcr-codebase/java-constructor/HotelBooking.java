abb478ed1c4917fd7ca0a9220a3f154a764702cc
sarthakpatel2 <sarthakpatelboby@gmail.com>
2025-12-31 15:21:19 +0530

    [Sarthak Patel] Add. Hotel Booking

---
+++ oops-practice/gcr-codebase/java-constructor/HotelBooking.java
@@ -0,0 +1,32 @@
+class HotelBooking{
+    String guestName;
+    String roomType;
+    int nights;
+    // Default constructor
+    HotelBooking(){
+        guestName="Guest";
+        roomType="Standard";
+        nights=1;
+    }
+    // Parameterized constructor
+    HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName=guestName;
+        this.roomType=roomType;
+        this.nights=nights;
+    }
+    // Copy constructor
+    HotelBooking(HotelBooking hb) {
+        this.guestName=hb.guestName;
+        this.roomType=hb.roomType;
+        this.nights=hb.nights;
+    }
+    void display() {
+        System.out.println(guestName+" | "+roomType+" | Nights: " +nights);
+    }
+    public static void main(String[] args){
+        HotelBooking b1=new HotelBooking();
+        HotelBooking b2=new HotelBooking(b1);
+        b1.display();
+        b2.display();
+    }
+}
