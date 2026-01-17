b8712931bf19a2acf92fac1c336fc7072b113548
sarthakpatel2 <sarthakpatelboby@gmail.com>
2026-01-17 11:18:39 +0530

    [Sarthak Patel] Add . First  negative

---
+++ dsa-practice/gcr-codebase/searching/FirstNegative.java
@@ -0,0 +1,19 @@
+public class FirstNegative{
+    public static int findFirstNegative(int[] arr){
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]<0){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int[] arr={5,3,7,-2,8,-9};
+        int index=findFirstNegative(arr);
+        if (index !=-1){
+            System.out.println("First negative number found at index: "+index);
+        }else{
+            System.out.println("No negative number found");
+        }
+    }
+}
