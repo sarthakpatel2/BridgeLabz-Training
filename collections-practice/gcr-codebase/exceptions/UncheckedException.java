638bb7942db2249664ea7d145b66cb0ce2214f7e
sarthakpatel2 <sarthakpatelboby@gmail.com>
2026-01-26 15:28:17 +0530

    [Sarthak Patel] Add . Unchecked Exception

---
+++ collections-practice/gcr-codebase/exceptions/UncheckedException.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class UncheckedException{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        try{
+            System.out.print("Enter numerator: ");
+            int a=sc.nextInt();
+            System.out.print("Enter denominator: ");
+            int b =sc.nextInt();
+            int result=a/b;
+            System.out.println("Result: "+result);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Error:- Division by zero is not allowed");
+        }
+        catch (InputMismatchException e){
+            System.out.println("Error:- Please enter valid numeric values");
+        }
+        finally {
+            sc.close();
+        }
+    }
+}
