4ac5591d5f0e2e219e3691e3036c9a3a69edda85
sarthakpatel2 <sarthakpatelboby@gmail.com>
2026-01-30 10:52:39 +0530

    [Sarthak Patel] Add . UserName Validator

---
+++ collections-practice/gcr-codebase/regex/ValidateUsername.java
@@ -0,0 +1,11 @@
+public class ValidateUsername{
+    public static boolean isValidUsername(String username){
+        String regex ="^[A-Za-z][A-Za-z0-9_]{4,14}$";
+        return username.matches(regex);
+    }
+    public static void main(String[] args){
+        System.out.println(isValidUsername("user_123"));
+        System.out.println(isValidUsername("123user"));
+        System.out.println(isValidUsername("us")); 
+    }
+}
