3cae57691e363a5fb1fdbc8b12d77a7c3ddf9d32
sarthakpatel2 <sarthakpatelboby@gmail.com>
2026-02-02 11:33:30 +0530

    [Sarthak Patel] Add . Filter Students

---
+++ collections-practice/gcr-codebase/CSV-handling/student/FilterStudents.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class FilterStudents{
+    public static void main(String[] args){
+        String filePath="collections-practice/gcr-codebase/CSV-handling/students.csv";
+        String line;
+        try (BufferedReader br=new BufferedReader(new FileReader(filePath))){
+            //skip header
+            br.readLine();
+            System.out.println("Students scoring more than 80: ");
+            System.out.println(" ");
+            while ((line=br.readLine())!=null){
+                String[] data=line.split(",");
+                int marks=Integer.parseInt(data[3]);
+                if (marks >80){
+                    System.out.println("ID: "+data[0]+", Name: "+data[1]+", Marks: "+marks);
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
