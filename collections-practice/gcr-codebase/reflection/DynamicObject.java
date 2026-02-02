f37875c852562a18dbbb589aeddae69a5b16ac1c
sarthakpatel2 <sarthakpatelboby@gmail.com>
2026-02-02 19:47:50 +0530

    [Sarthak Patel] Add . Dynamic Object

---
+++ collections-practice/gcr-codebase/reflection/DynamicObject.java
@@ -0,0 +1,10 @@
+public class DynamicObject{
+    public DynamicObject(){
+        System.out.println("Object created");
+    }
+    public static void main(String[] args)throws Exception {
+        Class<?> cls=Class.forName("DynamicObjectDemo");
+        Object obj=cls.getDeclaredConstructor().newInstance();
+        System.out.println("Class name: "+obj.getClass().getName());
+    }
+}
