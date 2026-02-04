55e71623d67349b3e8b8cdc4691b9e9abf1313ac
sarthakpatel2 <sarthakpatelboby@gmail.com>
2026-02-04 16:24:43 +0530

    [Sarthak Patel] Add . Password Validator

---
+++ java8/gcr-codebase/functional_interface/PasswordValidator.java
@@ -0,0 +1,27 @@
+interface SecurityUtils{
+    static boolean isStrongPassword(String password){
+        if (password==null || password.length()<8)
+            return false;
+        boolean hasUpper= false;
+        boolean hasLower=false;
+        boolean hasDigit=false;
+        boolean hasSpecial=false;
+        for (char ch : password.toCharArray()){
+            if (Character.isUpperCase(ch)) hasUpper=true;
+            else if (Character.isLowerCase(ch)) hasLower=true;
+            else if (Character.isDigit(ch)) hasDigit=true;
+            else hasSpecial=true;
+        }
+        return hasUpper && hasLower && hasDigit && hasSpecial;
+    }
+}
+public class PasswordValidator{
+    public static void main(String[] args){
+        String password="qwerty@123";
+        if (SecurityUtils.isStrongPassword(password)){
+            System.out.println("Strong password");
+        }else{
+            System.out.println("Weak password");
+        }
+    }
+}
