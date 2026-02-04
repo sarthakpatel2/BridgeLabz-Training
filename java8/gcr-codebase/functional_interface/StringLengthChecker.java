6bfce7d12e61f54626b1e780b60d7af4dfa4ac9b
sarthakpatel2 <sarthakpatelboby@gmail.com>
2026-02-04 16:00:14 +0530

    [Sarthak Patel] Add . String Length Checker

---
+++ java8/gcr-codebase/functional_interface/StringLengthChecker.java
@@ -0,0 +1,16 @@
+import java.util.function.Function;
+public class StringLengthChecker{
+    public static void main(String[] args){
+        int maxLimit=20;
+        //function (maps String to Integer)
+        Function<String, Integer>lengthCalculator=message ->message.length();
+        String message="I am Batman";
+        int length=lengthCalculator.apply(message);
+        if (length>maxLimit){
+            System.out.println("Message exceeds limit");
+        }else{
+            System.out.println("Message is within limit");
+        }
+        System.out.println("Message length: "+length);
+    }
+}
