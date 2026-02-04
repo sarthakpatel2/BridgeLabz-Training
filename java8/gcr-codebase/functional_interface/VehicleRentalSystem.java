446bcde83dcb3b2cbc783554f47469522e4c5773
sarthakpatel2 <sarthakpatelboby@gmail.com>
2026-02-04 15:40:53 +0530

    [Sarthak Patel] Add . Car Rental System

---
+++ java8/gcr-codebase/functional_interface/VehicleRentalSystem.java
@@ -0,0 +1,41 @@
+interface RentalVehicle{
+    void rent();
+    void returnVehicle();
+}
+class Car implements RentalVehicle {
+    public void rent(){
+        System.out.println("Car rented");
+    }
+    public void returnVehicle(){
+        System.out.println("Car returned");
+    }
+}
+class Bike implements RentalVehicle{
+    public void rent(){
+        System.out.println("Bike rented");
+    }
+    public void returnVehicle() {
+        System.out.println("Bike returned");
+    }
+}
+class Bus implements RentalVehicle{
+    public void rent(){
+        System.out.println("Bus rented");
+    }
+    public void returnVehicle(){
+        System.out.println("Bus returned");
+    }
+}
+public class VehicleRentalSystem{
+    public static void main(String[] args){
+        RentalVehicle v1=new Car();
+        RentalVehicle v2=new Bike();
+        RentalVehicle v3=new Bus();
+        v1.rent();
+        v2.rent();
+        v3.rent();
+        v1.returnVehicle();
+        v2.returnVehicle();
+        v3.returnVehicle();
+    }
+}
