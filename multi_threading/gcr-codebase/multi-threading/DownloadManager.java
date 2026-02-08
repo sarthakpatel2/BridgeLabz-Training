e4b8823d63d164bc1ba592c3ae12ddbaf45ed8c4
sarthakpatel2 <sarthakpatelboby@gmail.com>
2026-02-08 13:23:05 +0530

    [Sarthak Patel] Add . Download Manager

---
+++ multi_threading/gcr-codebase/multi-threading/DownloadManager.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+class FileDownloader extends Thread {
+    private String fileName;
+    private Random random=new Random();
+    public FileDownloader(String fileName){
+        this.fileName = fileName;
+    }
+    @Override
+    public void run() {
+        for (int i =0;i<=100;i+=25){
+            System.out.println("["+getName()+"] Downloading " +fileName+": "+i+"%");
+            try {
+                Thread.sleep(500+random.nextInt(500));
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+public class DownloadManager{
+    public static void main(String[] args) throws InterruptedException {
+        FileDownloader d1=new FileDownloader("Document.pdf");
+        FileDownloader d2=new FileDownloader("Image.jpg");
+        FileDownloader d3=new FileDownloader("Video.mp4");
+        d1.start();
+        d2.start();
+        d3.start();
+        d1.join();
+        d2.join();
+        d3.join();
+        System.out.println("All downloads complete");
+    }
+}
